ea7d05f4085f13e3e376408458082179a0799e16
VinaySD <158060394+VinaySD@users.noreply.github.com>
2026-07-30 22:14:13 +0530

    Create BasicLocators.java

---
+++ locators/BasicLocators.java
@@ -0,0 +1,44 @@
+package Selenium.Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BasicLocators {
+
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.youtube.com/");
+
+        // Name Locator
+        driver.findElement(By.name("search_query")).sendKeys("VSDAnimeX");
+
+        // ID Locator
+        boolean logoDisplayStatus = driver.findElement(By.id("logo-icon")).isDisplayed();
+        System.out.println("Logo Displayed: " + logoDisplayStatus);
+
+        // Submit
+        driver.findElement(By.name("search_query")).submit();
+
+        // Link Text Locator (Example)
+        // driver.findElement(By.linkText("Shorts")).click();
+
+        // Partial Link Text Locator (Example)
+        // driver.findElement(By.partialLinkText("Short")).click();
+
+        // Tag Name Locator
+        List<WebElement> headerLinks = driver.findElements(By.tagName("a"));
+        System.out.println("Total Header Links: " + headerLinks.size());
+
+        // Class Name Locator (Example)
+        // WebElement searchBox = driver.findElement(By.className("ytSearchboxComponentInput"));
+        // searchBox.clear();
+
+        driver.quit();
+    }
+}
