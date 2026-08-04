c23bbd92fd52d22d629cc02d37402fd0759e0495
VinaySD <158060394+VinaySD@users.noreply.github.com>
2026-08-04 08:20:27 +0530

    Add Shadow DOM handling example
    Add a Selenium example demonstrating Shadow DOM handling.
    
    Included example:
    - ShadowDomElements: access and interact with elements inside single, nested, and multi-level Shadow DOMs using getShadowRoot().
    
    Includes a README explaining Shadow DOM concepts and step-by-step examples.
    
    Example is organized under the shadow-dom package.

---
+++ shadow-dom/ShadowDomElements.java
@@ -0,0 +1,26 @@
+package day39;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ShadowDomElements {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://books-pwakit.appspot.com/");
+        driver.manage().window().maximize();
+
+        // driver.findElement(By.cssSelector("#input")).sendKeys("WELCOME"); // NoSuchElementException
+
+        // Handle shadow DOM element
+
+        
+        SearchContext shadow = driver.findElement(By.cssSelector("book-app[apptitle='BOOKS']")).getShadowRoot();
+        Thread.sleep(1000);
+        shadow.findElement(By.cssSelector("#input")).sendKeys("WELCOME");
+    }
+}
